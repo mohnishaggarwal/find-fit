f6c891972b988f2f0ce3ebb267dd359dbfa1ae2c
Marisa Wong <mariwong@umich.edu>
2021-11-15 17:46:36 -0500

    Changed class to className on homepage

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 function Home() {
     return (
         <div className='home'>
-            <div class='background-img'></div>
+            <div className='background-img'></div>
             <div className='home-container'>
                 <div className='home-header'>Welcome!</div>
                 <div className='home-description'>
