dec8d938bfc863edc15e620cc2b4e0ab9c1dcd0b
yurichen17 <68716357+yurichen17@users.noreply.github.com>
2021-11-19 00:20:11 -0500

    Changed myData[i] to item

--- src/screens/MatchingOptions.js
+++ src/screens/MatchingOptions.js
@@ -11,10 +11,34 @@ function MatchingOptions() {
         setRegimes(MatchingService.matchRegimes(qaState));
     }, [qaState]);
 
+    //regimes is not sorted, so I will try store into array then sort
+    //take the top 3 choices and display their links below
+    var myData=Object.keys(regimes).map(function(key){
+        return[key, regimes[key]];
+    });
+    myData = myData.sort(function(first, second){
+        return second[1][1]-first[1][1];
+    });
+    myData = myData.slice(0,3);
+    //score would be myData[i][1][1]
     return (
         <div>
             <p>This is the page where we give the user possible regimes</p>
-            <Link to="/questions">Change</Link>
+            <div>
+                <div>
+                    {myData.map(function(item, i){
+                        return <div>
+                            <div>
+                                <Link to="/regime">{item[1][0]}</Link>
+                            </div>
+                            <div>
+                                <p>Score={item[1][1]}</p>
+                            </div>
+                        </div>
+                    })}
+                </div>
+            </div>
+            <Link to="/questions">Change Answers</Link>
         </div>
     )
 }
