730ff345bbdd870f8034bb242786d7737c93892e
mohnishaggarwal <mohnisha@umich.edu>
2021-11-19 17:16:27 -0500

    firebase stuff initialized

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import firebase from "firebase/app";
+import "firebase/firestore";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyD_3V2-RyaZyVj9K3tYkqkYI_ZA7OICs_w",
+    authDomain: "find-fit-ce6dd.firebaseapp.com",
+    projectId: "find-fit-ce6dd",
+    storageBucket: "find-fit-ce6dd.appspot.com",
+    messagingSenderId: "157930178101",
+    appId: "1:157930178101:web:22b8d61871db480888537c",
+    measurementId: "G-TKH7R4WTGV"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+export default firebase;
