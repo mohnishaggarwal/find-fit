cdb33db39df09c64bd13b9dfc8c3066b2222a2f1
mohnishaggarwal <mohnisha@umich.edu>
2021-11-20 21:38:59 -0500

    Comment feature working:

--- src/components/Comments.js
+++ src/components/Comments.js
@@ -1,18 +1,50 @@
 import React, { useState, useEffect } from 'react';
 import CommentsService from '../services/CommentsService';
+import './comments.css'
 
 function Comments(regime) {
-
+    const [comments, setComments] = useState([]);
+    const [userComment, setUserComment] = useState("");
 
     useEffect(() => {
-        CommentsService.fetchComments(regime);
-        CommentsService.addComment(regime, "Cool regime!");
-    })
+        let regime = "bodybuilding";    // delete later
+        CommentsService.fetchComments(regime, setComments);
+    }, []);
+
+    function submitComment(event) {
+        let regime = "bodybuilding";    // delete later
+        event.preventDefault();
+        console.log(userComment);
+        CommentsService.addComment(regime, userComment);
+        setUserComment("");
+        CommentsService.fetchComments(regime, setComments);
+
+    }
+
+    function handleChange(event) {
+        setUserComment(event.target.value);
+    }
 
 
     return (
         <div className="comments-section">
-            
+            {
+                comments.map((comment, idx) => {
+                    return (
+                        <div key={idx} className="comment">
+                            <b>Anonymous:&nbsp;</b>{comment}
+                        </div>
+                    )
+                })
+            }
+            <form onSubmit={submitComment}>
+                <label>
+                    <b>Anonymous: &nbsp;</b>
+                    <input type="text" value={userComment} onChange={handleChange} />
+                </label>
+                <input type="submit" value="Submit" />
+            </form>
+
         </div>
     )
 }
--- src/services/CommentsService.js
+++ src/services/CommentsService.js
@@ -2,13 +2,12 @@ import { db } from "./../firebase";
 import { collection, getDocs, addDoc } from 'firebase/firestore';
 
 const CommentsService = {
-    fetchComments: async (regime) => {
+    fetchComments: async (regime, setComments) => {
         const regimesRef = collection(db, `regimes/${regime}/comments`);
         const snapshot = await getDocs(regimesRef);
-        const data = snapshot.docs.map((doc) => (
-            { ...doc.data()})
-        );
-        return data;
+        const data = snapshot.docs.map(doc => doc.data().comment);
+        console.log(data);
+        setComments(data);
     },
     addComment: async(regime, comment) => {
         const regimesRef = collection(db, `regimes/${regime}/comments`);
