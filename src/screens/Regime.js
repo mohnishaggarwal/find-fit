c1772465cd54d729cd9a7d38ffd8be431bd5cc60
situe <situe@umich.edu>
2021-11-22 22:37:35 -0500

    Added color to schedule and added descriptions for links

--- src/screens/Regime.js
+++ src/screens/Regime.js
@@ -6,7 +6,7 @@ const list_of_regimes = require('./../regimes.json')
 function Regime() {
     const location = useLocation()
     //const regime_type = "crossfit"
-    const regime_type = "running";
+    let regime_type = "crossfit";
     if(location.state){
         regime_type = location.state.type;
     };
@@ -148,7 +148,6 @@ function Regime() {
     return (
         <div className="regime-page">
             <h1 className="regime-title">We believe you should try {regime["name"]}!</h1>
-            <hr />
             <div className="regime-body">
                 <div className="regime-main-body">
                     <div className="regime-left">
@@ -166,8 +165,11 @@ function Regime() {
                         {
                             regime["links"].map((link,i) => {
                                 return(
-                                <div className="regime-link">
-                                    <a href={link} key={i}>{link} </a>
+                                <div className="regime-link-section">
+                                    <p>{link["description"]}</p>
+                                    <p>
+                                        <a href={link["link"]} key={i}>{link["link"]} </a> 
+                                    </p>
                                 </div>
                                 )
                             })
